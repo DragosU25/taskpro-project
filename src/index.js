d39fc5266ee97ff2fb6ccbe35c9d33cfa8f324cc
Dragos Ungureanu <ungureanud6@gmail.com>
2025-01-15 12:41:57 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/taskpro-project/">
+      <BrowserRouter basename="/taskpro-project">
         <App />
       </BrowserRouter>
     </Provider>
