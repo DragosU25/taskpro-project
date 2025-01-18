b750b93fe5cc8e0d2da70df3c02eff383178ca80
Dragos Ungureanu <ungureanud6@gmail.com>
2025-01-18 22:31:02 +0100

    Updates

---
+++ static/js/592.54fc6ae7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktaskpro_project=self.webpackChunktaskpro_project||[]).push([[592],{592:(e,r,s)=>{s.r(r),s.d(r,{default:()=>a});s(43);var t=s(579);const a=function(){return(0,t.jsx)("div",{children:"DashboardPage"})}}}]);
+//# sourceMappingURL=592.54fc6ae7.chunk.js.map
