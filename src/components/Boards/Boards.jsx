5c1470a3fb482cb442bdeb5a21175546d2a1f7ba
Dragos Ungureanu <ungureanud6@gmail.com>
2025-01-21 21:28:47 +0100

    Update Boards.jsx

--- src/components/Boards/Boards.jsx
+++ src/components/Boards/Boards.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./Boards.module.css";
 import Icon from "../common/SvgIcon/SvgIcon";
 import Button from "../common/Button/Button";
